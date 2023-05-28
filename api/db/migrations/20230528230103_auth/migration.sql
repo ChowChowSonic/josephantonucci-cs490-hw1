c65b89497769c121fcd237ab884aadf9a78e31be
Joseph Antonucci <joeyant1367@gmail.com>
2023-05-28 19:33:27 -0400

    Chapter 4 commit

---
+++ api/db/migrations/20230528230103_auth/migration.sql
@@ -0,0 +1,37 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `message` on the `Contact` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `Contact` table. All the data in the column will be lost.
+  - Added the required column `body` to the `Contact` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `title` to the `Contact` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" DATETIME
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Contact" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" DATETIME?
+);
+INSERT INTO "new_Contact" ("createdAt", "email", "id") SELECT "createdAt", "email", "id" FROM "Contact";
+DROP TABLE "Contact";
+ALTER TABLE "new_Contact" RENAME TO "Contact";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
